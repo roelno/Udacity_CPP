965c3b9df3816c8208d4d93bfffe60b4727b28c0
Ro <lichtseeker@gmail.com>
2023-08-25 14:17:44 -0700

    sizeof()

---
+++ ch01_basics/02_print_variable.cpp
@@ -0,0 +1,17 @@
+#include "iostream"
+
+using namespace std;
+
+int main() {
+    int integer = 4543;
+    cout << "The value of integer is " << integer << "\n";
+
+    cout << "The size of int: " << sizeof(int) << endl;
+    cout << "The size of short: " << sizeof(short) << endl;
+    cout << "The size of long: " << sizeof(long) << endl;
+    cout << "The size of char: " << sizeof(char) << endl;
+    cout << "The size of foat: " << sizeof(float) << endl;
+    cout << "The size of double: " << sizeof(double) << endl;
+    cout << "The size of bool: " << sizeof(bool) << endl;
+    return 0;
+}
